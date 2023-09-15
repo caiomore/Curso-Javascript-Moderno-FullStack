f32c88e0e4ed28f5b3f75b47b56a915b2bb5e88d
caiomore <caio.workprofile@gmail.com>
2023-09-15 13:00:36 -0300

    first commit

---
+++ Inicio/comentario.js
@@ -0,0 +1,12 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log('Bom dia!');
+console.log('Boa tarde!');
+console.log('Boa noite!');
+// console.log('Javascript!');
+
+/*
+
+Comentários com
+mais de uma linha
+
+*/
